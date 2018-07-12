9bb77a661789cff7b9d114233f3a2424a47b3be2
KkdhH <40253804+KkdhH@users.noreply.github.com>
2018-07-12 20:44:02 +0900

    change
    company수정

--- company/company.js
+++ company/company.js
@@ -39,3 +39,26 @@ $('.business nav ul img').hover(function() {
 }, function() {
 	$(this).stop().css({'transform':'scale(1)', 'opacity':'1'});
 });
+
+
+
+$(".animsition").animsition({
+    inClass: 'fade-in-down-sm',
+    inDuration: 1500,
+    linkElement: '.animsition-link',
+    // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
+    loading: true,
+    loadingParentElement: '.wrap', //animsition wrapper element
+    loadingClass: 'animsition-loading',
+    loadingInner: '', // e.g '<img src="loading.svg" />'
+    timeout: false,
+    timeoutCountdown: 5000,
+    onLoadEvent: true,
+    browser: [ 'animation-duration', '-webkit-animation-duration'],
+    // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+    // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+    overlay : false,
+    overlayClass : 'animsition-overlay-slide',
+    overlayParentElement : 'body',
+    transition: function(url){ window.location.href = url; }
+  });
